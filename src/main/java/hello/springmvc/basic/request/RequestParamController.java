f6c8f059333703cbc9b0bf9861528eb57a4683ce
newseoyun <newseoyun@gmail.com>
2021-07-01 13:09:28 +0900

    .

--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -70,6 +71,14 @@ public class RequestParamController {
             @RequestParam(required = false, defaultValue = "-1") Integer age) {
         log.info("username={}, age={}", username, age);
 
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-map")
+    public String requestParamMap(
+            @RequestParam Map<String, Object> paramMap){
+        log.info("username={}, age={}", paramMap.get("username"), paramMap.get("age"));
         return "ok";
     }
 
